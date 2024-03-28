6ba166fe8f573b26694df0bfbba1fc22b8084f87
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-03-27 20:48:28 -0700

    Update db_connect.php

--- templates/db_connect.php
+++ templates/db_connect.php
@@ -9,7 +9,7 @@ $database = "db_25141755";
 $user = "25141755";
 $password = "25141755";
 
-$connection = mysqli_connect($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 $error = mysqli_connect_error();
 if($error != null)
