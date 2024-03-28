2139076a933e00d15a4e51196be8701834989a8b
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-03-28 16:43:31 -0700

    Update addproduct.php

--- templates/addproduct.php
+++ templates/addproduct.php
@@ -7,26 +7,18 @@
     $username = "25141755";  
     $password = "25141755"; 
     $dbname = "db_25141755"; 
-
-    // Create connection
     $connection = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
     if (mysqli_connect_error()) {
         echo "<script>alert('Unable to connect to database!'); window.location.href = 'home.jsp';</script>";
         exit();
     }
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Retrieve and sanitize input data
-        $productName = mysqli_real_escape_string($connection, $_POST['productName']);
-        $productPrice = mysqli_real_escape_string($connection, $_POST['productPrice']);
-        $description = mysqli_real_escape_string($connection, $_POST['description']);
-
-        // Insert data into the 'product' table
-        $sql = "INSERT INTO product (productName, price, description) VALUES ('$productName', '$productPrice', '$description')";
-
-        if (mysqli_query($connection, $sql)) {
+        $productName = ($connection, $_POST['productName']);
+        $productPrice = ($connection, $_POST['productPrice']);
+        $description = ($connection, $_POST['description']);
+        $add = "INSERT INTO product (productName, price, description) VALUES ('$productName', '$productPrice', '$description')";
+        if (mysqli_query($connection, $add)) {
             echo "<script>alert('New product added successfully.'); window.location.href = 'home.jsp';</script>";
         } else {
             echo "<script>alert('Error adding product: " . mysqli_error($connection) . "'); window.location.href = 'home.jsp';</script>";
