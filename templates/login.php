e5361e4e634610ec59476600adb7c7d72d570aee
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-03-27 21:24:36 -0700

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -3,11 +3,12 @@
 <html>
 <body>
     <?php
+        session_start();
         $servername = "localhost";
         $username = "25141755";
         $password = "25141755";
         $dbname = "db_25141755";
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $connection = new mysqli($servername, $username, $password, $dbname);
         if(mysqli_connect_error()) {
             echo "<p>Unable to connect to database!</p>";
             echo "<p><a href='javascript:history.back()'>go back</a></p>";
