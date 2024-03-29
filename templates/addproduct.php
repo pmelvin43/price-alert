680349d5f5dbd9ad8f985bb131c2fcdd08e6dc51
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-03-28 17:25:24 -0700

    Update addproduct.php

--- templates/addproduct.php
+++ templates/addproduct.php
@@ -3,6 +3,10 @@
 <html>
 <body>
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
         $servername = "localhost";
         $username = "25141755";
         $password = "25141755";
