4672a1d345e98383b53a21f77c993c4a48503137
Noah <113081068+yiuunamm@users.noreply.github.com>
2024-03-28 17:35:59 -0700

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <body>
@@ -9,35 +8,35 @@
         $password = "25141755";
         $dbname = "db_25141755";
         $connection = new mysqli($servername, $username, $password, $dbname);
-           if(mysqli_connect_error()) {
-        echo "<p>Unable to connect to database!</p>";
-        exit();
-    }
-    if ($_SERVER["REQUEST_METHOD"]=="POST") {
-        if (isset($_POST["username"]) && isset($_POST["password"])) {
-            $username = mysqli_real_escape_string($connection,$_POST["username"]);
-            $password = mysqli_real_escape_string($connection,$_POST["password"]);
 
-            $hashed_password = md5($password);
-            $login="SELECT * FROM users WHERE username = '$username' AND password ='$hashed_password'";
-            $result=mysqli_query($connection, $login);
+        if(mysqli_connect_error()) {
+            echo "<p>Unable to connect to database!</p>";
+            exit();
+        }
 
-            if($result&&mysqli_num_rows($result) > 0) {
-                echo "<p>User has a valid account.</p>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["username"]) && isset($_POST["password"])) {
+                $username = mysqli_real_escape_string($connection, $_POST["username"]);
+                $password = mysqli_real_escape_string($connection, $_POST["password"]);
+
+                $hashed_password = md5($password);
+                $login = "SELECT * FROM users WHERE username = '$username' AND password = '$hashed_password'";
+                $result = mysqli_query($connection, $login);
+
+                if ($result && mysqli_num_rows($result) > 0) {
+                    echo "<script>alert('User has a valid account.'); window.location.href = 'home.php';</script>";
+                } else {
+                    echo "<script>alert('Invalid username and/or password.'); window.location.href = 'home.php';</script>";
+                }
             } else {
-                echo "<p>Invalid username and/or password.</p>";
+                echo "<p>Username and password required.</p>";
                 echo "<p><a href='javascript:history.back()'>Return to Login</a></p>";
             }
         } else {
-            echo "<p>Username and password required.</p>";
-            echo "<p><a href='javascript:history.back()'>Return to Login</a></p>";
+            echo "<p>Invalid request.</p>";
         }
-    } 
-    else {
-        echo "<p>Invalid request.</p>";
-    }
 
-    mysqli_close($connection);
+        mysqli_close($connection);
     ?>
 </body>
 </html>
