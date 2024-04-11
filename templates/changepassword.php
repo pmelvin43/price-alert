4cbbe2cf473716eaeb27dcf15dd79a9145965099
ZZD-no1 <92241294+ZZD-no1@users.noreply.github.com>
2024-04-10 23:25:43 -0700

    updated

--- templates/changepassword.php
+++ templates/changepassword.php
@@ -1,57 +1,54 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $servername = "localhost";
-        $username = "25141755";
-        $password = "25141755";
-        $dbname = "db_25141755";
-        $connection = new mysqli($servername, $username, $password, $dbname);
-        if(mysqli_connect_error()) {
-            echo "<p>Unable to connect to database!</p>";
-            echo "<p><a href='javascript:history.back()'>Return to User Entry</a></p>";
-            exit();
-        }
-        if(isset($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['email'], $_POST['password'])) {
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $checkQuery = "SELECT * FROM users WHERE username = '$username' OR email = '$email' LIMIT 1";
-            $result = mysqli_query($connection, $checkQuery);
-            if($result) {
-                $user = mysqli_fetch_assoc($result);
-                if ($user) {
-                    if ($user['username'] === $username || $user['email'] === $email) {
-                        echo "<p>User already exists with this name and/or email.</p>";
-                        echo "<p><a href='javascript:history.back()'>Return to User Entry</a></p>";
-                    }
-                } else {
-                    $hashed_password = md5($password);
-                    $insert = "INSERT INTO users (firstname, lastname, username, email, password) VALUES ('$firstname', '$lastname', '$username', '$email', '$hashed_password')";
+<?php
 
-                    if(mysqli_query($connection, $insert)) {
-                        echo "<p><a href='javascript:history.back()'>Login</a></p>";
-                    } else {
-                        echo "<p>Error: " . mysqli_error($connection) . "</p>";
-                        echo "<p><a href='javascript:history.back()'>Return to User Entry</a></p>";
-                    }
-                }
-            } else {
-                echo "<p>Error checking user existence.</p>";
-                echo "<p><a href='javascript:history.back()'>Return to User Entry</a></p>";
-            }
-            mysqli_free_result($result);
-            mysqli_close($connection);
-        } else {
-            echo "<p>Error: All fields are required.</p>";
-            echo "<p><a href='javascript:history.back()'>Return to User Entry</a></p>";
-        }
-    } else {
-        echo "<p>Invalid request.</p>";
-    }
-    ?>
-</body>
-</html>
+// Database connection details
+require_once 'db_connect.php';
+
+// Check for POST request
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    // Bad data handling/closing
+    die("This page only accepts POST requests.");
+}
+
+$username = mysqli_real_escape_string($connection, $_POST['username']);
+// Hashing old password with md5
+$oldPassword = md5($_POST['old-password']);
+// Hashing new password with md5
+$newPassword = md5($_POST['password']);
+
+// Verify old password in DB
+$stmt = $connection->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $username, $oldPassword);
+$stmt->execute();
+
+$result = $stmt->get_result();
+if ($result->num_rows == 0) {
+    echo '<script type="text/javascript">';
+    echo 'alert("wrong old password");';
+    echo 'window.location.href="changepassword.html";';
+    echo '</script>';
+    die();
+}
+
+// Update to new password
+$stmt = $connection->prepare("UPDATE users SET password = ? WHERE username = ?");
+$stmt->bind_param("ss", $newPassword, $username);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo '<script type="text/javascript">';
+    echo 'alert("password changed");';
+    echo 'window.location.href="login.html";';
+    echo '</script>';
+    die();
+} else {
+    echo '<script type="text/javascript">';
+    echo 'alert("password update failed");';
+    echo 'window.location.href="changepassword.html";';
+    echo '</script>';
+    die();
+}
+
+// Close Database
+mysqli_close($connection);
+
+?>
--- templates/db_connect.php
+++ templates/db_connect.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <?php
-        $servername = "localhost";
-        $username = "25141755";
-        $password = "25141755";
-        $dbname = "db_25141755";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if(mysqli_connect_error()) {
-            echo "<p>Unable to connect to database!</p>";
-            echo "<p><a href='javascript:history.back()'>go back</a></p>";
-            exit();
-        }
+
+
+<?php
+
+$host = "localhost";
+$database = "price-alert";
+$user = "root";
+$password = "";
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+$error = mysqli_connect_error();
+if($error != null)
+{
+  $output = "<p>Unable to connect to database!</p>";
+  exit($output);
+}
+
+?>
+</html>
