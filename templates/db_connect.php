14d388d7fefb1c167de3a1029b4bcb07c4077dee
ZZD-no1 <92241294+ZZD-no1@users.noreply.github.com>
2024-04-11 01:42:24 -0700

    Update db_connect.php

--- templates/db_connect.php
+++ templates/db_connect.php
@@ -4,21 +4,18 @@
 
 <?php
 
-$connection = mysqli_connect("localhost","26780833","26780833","db_26780833");
-### OR
-$servername = "localhost"; 
+$servername = "localhost";
 $username = "26780833";
-$password = "26780833"; 
-$dbname = "db_26780833"; 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname)
+$password = "26780833";
+$dbname = "db_26780833";
+$connection = new mysqli($servername, $username, $password, $dbname);
 
-$error = mysqli_connect_error();
-if($error != null)
-{
-  $output = "<p>Unable to connect to database!</p>";
-  exit($output);
+if(mysqli_connect_error()) {
+    echo "<p>Unable to connect to database!</p>";
+    exit();
 }
+
+
 
 ?>
 </html>
