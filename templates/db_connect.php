2d8fedad7c40ccdd3e0817df075b0af024f0adf3
ZZD-no1 <92241294+ZZD-no1@users.noreply.github.com>
2024-04-11 18:08:38 -0700

    Update db_connect.php

--- templates/db_connect.php
+++ templates/db_connect.php
@@ -7,7 +7,7 @@
 $servername = "localhost";
 $username = "26780833";
 $password = "26780833";
-$dbname = "db_26780833";
+$dbname = "db_25141755";
 $connection = new mysqli($servername, $username, $password, $dbname);
 
 if(mysqli_connect_error()) {
